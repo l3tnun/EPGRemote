b598d1514a83bf4a3a94ed76bac7590e08416697
l3tnun <l3tnun@gmail.com>
2016-01-29 15:22:28 +0900

    epgrecRecordedTag の整理

--- nodejs/requestHandlers/epgrecRecordedTag.js
+++ nodejs/requestHandlers/epgrecRecordedTag.js
@@ -7,65 +7,35 @@ module.exports = function(response, parsedUrl) {
     log.access.info("Request handler 'epgrec recorded tag' was called.");
 
     var viewFunction = {
-        "KW" : { "sql" : sqlModel.getRecordedKeywordList, "view" : keywordTagView },
-        "CH" : { "sql" : sqlModel.getRecordedChannelList, "view" : channelTagView }
+        "keyword" : { "sql" : sqlModel.getRecordedKeywordList, "id" : "autorec" ,"name" : "keyword", "noId" : "予約言なし" },
+        "channel" : { "sql" : sqlModel.getRecordedChannelList, "id" : "channel_id", "name" : "name", "noId" : "放送局なし" }
     };
 
     var type;
-    if(typeof parsedUrl.query.type == "undefined") { type = "KW"; }
+    if(typeof parsedUrl.query.type == "undefined") { type = "keyword"; }
     else { type = parsedUrl.query.type; }
 
     if(typeof viewFunction[type] == "undefined") { notFound(response); return; }
-    viewFunction[type].sql(15, parsedUrl.query.num, function(results) { viewFunction[type].view(response, parsedUrl, results) });
-}
+    viewFunction[type].sql(15, parsedUrl.query.num, function(results) {
+        if(results == '') { notFound(response); return; }
+        var tagList = {}
+        results[0].forEach(function(result) { tagList[result.id] = { "name" : result[viewFunction[type].name], "cnt" : 0 } });
 
-function keywordTagView(response, parsedUrl, results) {
-    if(results == '') { notFound(response); return; }
+        results[1].forEach(function(result) {
+            if(typeof tagList[result[viewFunction[type].id]] == "undefined" ) {
+                tagList[result[viewFunction[type].id]] = { "name" : viewFunction[type].noId, "cnt" : 0 };
+            }
+            tagList[result[viewFunction[type].id]].cnt += 1;
+        });
 
-    var keywordList = {}
-    results[0].forEach(function(result) {
-        keywordList[result.id] = { "keyword" : result.keyword, "cnt" : 0 }
-    });
-    keywordList[0] = { "keyword" : "予約語なし", "cnt" : 0 }
-
-    results[1].forEach(function(result) {
-        //autorec == keyword.id
-        if(typeof keywordList[result.autorec] == "undefined") {
-            keywordList[0].cnt += 1;
-        } else {
-            keywordList[result.autorec].cnt += 1;
+        var tags = [];
+        for (var key in tagList) {
+            if(tagList[key].cnt != 0) {
+                tags.push({ "type" : type, "link" : key, "title" : tagList[key].name, "cnt" : tagList[key].cnt });
+            }
         }
+
+        viewer.epgrecRecordedTag(response, tags);
     });
-
-    var keywords = [];
-
-    for (var key in keywordList) {
-        keywords.push({ "autorec" : key, "keyword" : keywordList[key].keyword, "cnt" : keywordList[key].cnt });
-    }
-
-    viewer.epgrecRecordedKeywordsTag(response, keywords);
-}
-
-function channelTagView(response, parsedUrl, results) {
-    if(results == '') { notFound(response); return; }
-
-    var channelList = {}
-    results[0].forEach(function(result) {
-        channelList[result.id] = { "channelName" : result.name, "cnt" : 0 }
-    });
-
-    results[1].forEach(function(result) {
-        channelList[result.channel_id].cnt += 1;
-    });
-
-    var channels = [];
-
-    for (var key in channelList){
-        if(channelList[key].cnt != 0) {
-            channels.push({ "channel_id" : key, "channelName" : channelList[key].channelName, "cnt" : channelList[key].cnt });
-        }
-    }
-
-    viewer.epgrecRecordedChannelTag(response, channels);
 }
 
