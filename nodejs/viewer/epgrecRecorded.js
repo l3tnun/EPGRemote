cd56c6a93776ce66f865708279994cb95b2a5062
l3tnun <l3tnun@gmail.com>
2016-01-30 10:29:38 +0900

    アクションメニューを追加

--- nodejs/viewer/epgrecRecorded.js
+++ nodejs/viewer/epgrecRecorded.js
@@ -2,14 +2,17 @@ var readFile = require(__dirname + "/readFile");
 var responseFile = require(__dirname + "/responseFile");
 
 module.exports = function(response, programs) {
+    var actionId = 0;
     var programStr = ""
     programs.forEach(function(program) {
-        programStr += `<li data-icon="delete"><a href="${program.videLink}" target="_self">`
+        programStr += `<li data-icon="action"><a href="${program.videLink}" target="_self">`
         programStr += `<img src="${program.thumbs}">`
         programStr += `<h3>${program.title}</h3>`
         programStr += `<p>${program.info}</p>`
         programStr += `<p>${program.description}</p>`
-        programStr += `</a><a href="javascript:openDeleteDialog(${program.id}, '${program.title}')"></a></li>`
+        //programStr += `</a><a href="javascript:openDeleteDialog(${program.id}, '${program.title}')"></a></li>`
+        programStr += `</a><a id="actionId${actionId}" href="#" programId="${program.id}" programTitle="${program.title}" class="openActionMenu"></a></li>`
+        actionId += 1;
     });
 
     var htmlfile = readFile("./HTML/epgrecrecorded.html");
