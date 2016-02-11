c9f7da5c5dabd630a86e3d18d432ddcf518ce2af
l3tnun <l3tnun@gmail.com>
2016-02-11 22:24:13 +0900

    fix

--- nodejs/socketIoServerModule/epgrecProgram/getJumpChannelConfig.js
+++ nodejs/socketIoServerModule/epgrecProgram/getJumpChannelConfig.js
@@ -1,10 +1,13 @@
-var log = require(__dirname + "/../..//logger").getLogger();
+var log = require(__dirname + "/../../logger").getLogger();
+var util = require(__dirname + "/../../util");
 var tunerManager = require(__dirname + "/../../tunerManager");
 
 module.exports = function(io, socket) {
     //チューナー, ビデオサイズ等の設定を取得
     socket.on("getJumpChannelConfig", function (socketId, type) {
         log.access.info(`socketio 'getJumpChannelConfig' was called ${type}`);
+
+        if(util.getConfig()["useHLS"] == false) { return; }
 
         var tunerList = tunerManager.getActiveTuner(type);
         var videoConfig = tunerManager.getVideoSize();
