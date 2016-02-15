2f6788f31cf287e5aca3edc1b368f5fc77a7c7ac
l3tnun <l3tnun@gmail.com>
2016-02-15 18:54:07 +0900

    socketid の生成方法を戻した

--- js/new_socketio.js
+++ js/new_socketio.js
@@ -1,2 +1,2 @@
 var socketio = io.connect(window.location.protocol + "//" + window.location.host);
-var socketid = socketio.json.id;
+var socketid = `${new Date().getTime()}:${Math.random().toString(36).slice(-8)}`;
