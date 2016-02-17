534bda5c1c5f05ec9a923fc942f36d1c9965acde
l3tnun <l3tnun@gmail.com>
2016-02-17 18:57:59 +0900

    bfcache を無効にした

--- js/reconnect_socketio.js
+++ js/reconnect_socketio.js
@@ -1,11 +1,28 @@
 var connectStatus = true;
+var movePage = false;
 socketio.on('connect',function() {
     if(connectStatus == false) { location.reload(true); }
 });
 
 socketio.on('disconnect', function() {
-    $('#busy').css("visibility", "visible");
-    $.growl.error({ message: "接続が切断されました。" });
+    if(movePage == false) {
+        $('#busy').css("visibility", "visible");
+        $.growl.error({ message: "接続が切断されました。" });
+    }
     connectStatus = false;
 });
+
+window.onunload = function() {};
+
+window.onpageshow = function(event) {
+  if (event.persisted) {
+    window.location.reload();
+  }
+};
+
+window.onbeforeunload = function(event){
+    event = event || window.event;
+    connectStatus = true;
+    movePage = true;
+}
 
