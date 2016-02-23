72fcc2b9d1f7b7a4a9dec946b198c8c05da861e9
l3tnun <l3tnun@gmail.com>
2016-02-23 15:27:17 +0900

    fix fixPopupInput.js

--- js/fixPopupInput.js
+++ js/fixPopupInput.js
@@ -1,24 +1,24 @@
-$(window).resize(function () {
-    if($(window).height() > orgWindowHeightForPopup) {
-        orgWindowHeightForPopup = $(window).height();
-    }
-
-    if(windowHeightForPopup > $(window).height()) {
-        var tag = $(':focus').prop("tagName");
-        if(tag == "INPUT") {
-            windowKeyboardHeightForPopup = $(window).height() / 2 - orgWindowHeightForPopup / 10;
-        } else if(tag == "TEXTAREA") {
-            windowKeyboardHeightForPopup = $(window).height() / 2 - orgWindowHeightForPopup / 2;
-        }
-        $(".ui-popup-active").css("top", windowKeyboardHeightForPopup + "px");
-    } else if($(".ui-popup-active").height() != null) {
-        $(".ui-popup-active").css("top", (($(window).height() - $(".ui-popup-active").height()) / 2) + "px");
-    }
-});
-
-var windowHeightForPopup, orgWindowHeightForPopup, windowKeyboardHeightForPopup = 0;
 $(function() {
-    windowHeightForPopup = $(window).height();
-    orgWindowHeightForPopup = $(window).height();
+    var windowHeightForPopup = windowHeightForPopup = $(window).height();
+    var orgWindowHeightForPopup = orgWindowHeightForPopup = $(window).height();
+
+    $(window).resize(function () {
+        if($(window).height() > orgWindowHeightForPopup) {
+            orgWindowHeightForPopup = $(window).height();
+        }
+
+        if(windowHeightForPopup > $(window).height()) {
+            var tag = $(':focus').prop("tagName");
+            var windowKeyboardHeightForPopup = 0
+            if(tag == "INPUT") {
+                windowKeyboardHeightForPopup = $(window).height() / 2 - orgWindowHeightForPopup / 10;
+            } else if(tag == "TEXTAREA") {
+                windowKeyboardHeightForPopup = $(window).height() / 2 - orgWindowHeightForPopup / 2;
+            }
+            $(".ui-popup-active").css("top", windowKeyboardHeightForPopup + "px");
+        } else if($(".ui-popup-active").height() != null) {
+            $(".ui-popup-active").css("top", (($(window).height() - $(".ui-popup-active").height()) / 2) + "px");
+        }
+    });
 });
 
