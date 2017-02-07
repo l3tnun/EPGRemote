9a8a82880a81864c7d54a0db308434920b6dac97
l3tnun <l3tnun@gmail.com>
2017-02-07 20:33:15 +0900

    fix KeywordController

--- src_client/Controller/Keyword/KeywordController.ts
+++ src_client/Controller/Keyword/KeywordController.ts
@@ -9,6 +9,7 @@ class KeywordController extends ParentPageController {
 
     //ViewModel 初期化
     public initModel(): void {
+        super.initModel();
         this.viewModel = <KeywordViewModel>this.getModel("KeywordViewModel");
         this.viewModel.init();
         this.viewModel.update();
@@ -25,8 +26,8 @@ class KeywordController extends ParentPageController {
     }
 
     //ページから離れるときに呼び出される
-    public onunload(): void {
-        super.onunload();
+    public onRemove(): void {
+        super.onRemove();
         window.removeEventListener('resize', this.resizeListener, false );
     }
 
