9886fe8643a21b1c8f33a3faf474c78552d9bf4a
l3tnun <l3tnun@gmail.com>
2017-02-08 18:39:31 +0900

    fix

--- src_client/View/Search/SearchOptionView.ts
+++ src_client/View/Search/SearchOptionView.ts
@@ -300,7 +300,7 @@ class SearchOptionView extends SearchOptionBaseView {
             //検索ボタン
             m("button", {
                 class: "no-hover mdl-button mdl-js-button mdl-button--primary",
-                onclick: () => { this.viewModel.search(); }
+                onclick: () => { setTimeout(() => { this.viewModel.search(); }, 500); }
             }, "検索" ),
 
             //キャンセル or 元に戻すボタン
