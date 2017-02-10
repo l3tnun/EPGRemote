b2921498705c6776f87be88a00100bfb34693edd
l3tnun <l3tnun@gmail.com>
2017-02-10 15:24:51 +0900

    mobile での web app 化の方法を追加

--- src_client/View/TopPage/TopPageView.ts
+++ src_client/View/TopPage/TopPageView.ts
@@ -2,6 +2,7 @@
 
 import * as m from 'mithril';
 import View from '../View';
+import Util from '../../Util/Util';
 import NavigationComponent from '../../Component/Navigation/NavigationComponent';
 import HeaderComponent from '../../Component/Header/HeaderComponent';
 
@@ -16,7 +17,16 @@ class TopPageView extends View {
     public execute(): Mithril.Vnode<any, any> {
         return m("div", {
             class: "mdl-layout mdl-js-layout mdl-layout--fixed-header",
-            oncreate: () => { setTimeout(() => { this.openNavigation(); }, 200); },
+            oncreate: () => {
+                setTimeout(() => { this.openNavigation(); }, 200);
+
+                //web app 化
+                if(typeof m.route.param("mobile") == "undefined") { return; }
+                let meta = document.createElement("meta");
+                meta.setAttribute("name", Util.uaIsiOS() ? "apple-mobile-web-app-capable" : "mobile-web-app-capable");
+                meta.setAttribute("content", "yes");
+                document.getElementsByTagName("head")[0].appendChild(meta);
+            },
         }, [
             m(<Mithril.Component<{ title: string; }, {}>>this.headerComponent, { title: "EPGRemote" }),
             m(this.navigationComponent)
